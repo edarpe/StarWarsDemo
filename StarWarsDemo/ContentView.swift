12fff5e5e71818b1d54f9cd3434433f4a7039a43
Edwin Perez <edwin@edarpe.com>
2024-10-29 19:05:49 -0600

    Initial Commit

---
+++ StarWarsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsDemo
+//
+//  Created by Armando Perez on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarWarsDemo/StarWarsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsDemoApp.swift
+//  StarWarsDemo
+//
+//  Created by Armando Perez on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
